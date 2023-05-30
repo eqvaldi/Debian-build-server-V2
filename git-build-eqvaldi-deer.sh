bae126d96480d5c42ad1aec8e611bee8ab052b87
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2023-05-30 16:23:19 +0200

    Add files via upload

--- git-build-eqvaldi-deer.sh
+++ git-build-eqvaldi-deer.sh
@@ -6,38 +6,46 @@ cd xfce/
 mkdir EDGE
 mkdir X86
 mkdir ARM
+mkdir JETSON
 cd ..
 mkdir lxde
 cd lxde/
 mkdir EDGE
 mkdir X86
 mkdir ARM
+mkdir JETSON
 cd ..
 mkdir mate
 cd mate/
 mkdir EDGE
 mkdir X86
 mkdir ARM
+mkdir JETSON
 cd ..
 mkdir cinnamon
 cd cinnamon/
 mkdir EDGE
 mkdir X86
 mkdir ARM
+mkdir JETSON
 cd ..
 mkdir server
 cd server/
 mkdir EDGE
 mkdir X86
 mkdir ARM
+mkdir JETSON
 cd ..
 cd ..
 git clone https://github.com/Eqvaldi-deer/build.git
+cp ./buildscripts/compress.sh ./img
 cp ./buildscripts/bld-boards.sh ./build
 cp ./buildscripts/cinnamon.sh ./build
 cp ./buildscripts/x86.sh ./build
 cp ./buildscripts/edge.sh ./build
+cp ./buildscripts/jetson.sh ./build
 chmod +x ./build/bld-boards.sh
 chmod +x ./build/cinnamon.sh
 chmod +x ./build/x86.sh
 chmod +x ./build/edge.sh
+chmod +x ./build/jetson.sh
--- git-build-latest.sh
+++ git-build-latest.sh
@@ -12,32 +12,39 @@ cd lxde/
 mkdir EDGE
 mkdir X86
 mkdir ARM
+mkdir JETSON
 cd ..
 mkdir mate
 cd mate/
 mkdir EDGE
 mkdir X86
 mkdir ARM
+mkdir JETSON
 cd ..
 mkdir cinnamon
 cd cinnamon/
 mkdir EDGE
 mkdir X86
 mkdir ARM
+mkdir JETSON
 cd ..
 mkdir server
 cd server/
 mkdir EDGE
 mkdir X86
 mkdir ARM
+mkdir JETSON
 cd ..
 cd ..
 git clone --depth=1 --branch=main https://github.com/armbian/build
+cp ./buildscripts/compress.sh ./img
 cp ./buildscripts/bld-boards.sh ./build
 cp ./buildscripts/cinnamon.sh ./build
 cp ./buildscripts/x86.sh ./build
 cp ./buildscripts/edge.sh ./build
+cp ./buildscripts/jetson.sh ./build
 chmod +x ./build/bld-boards.sh
 chmod +x ./build/cinnamon.sh
 chmod +x ./build/x86.sh
 chmod +x ./build/edge.sh
+chmod +x ./build/jetson.sh
