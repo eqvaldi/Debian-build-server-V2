2dec2dbb0bcb6157ed05a0159a06c66786d377b9
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2023-07-17 15:28:03 +0200

    Add files via upload

--- update.sh
+++ update.sh
@@ -7,9 +7,7 @@ cp ./buildscripts/bld-boards.sh ./build
 cp ./buildscripts/cinnamon.sh ./build
 cp ./buildscripts/x86.sh ./build
 cp ./buildscripts/edge.sh ./build
-cp ./buildscripts/jetson.sh ./build
 chmod +x ./build/bld-boards.sh
 chmod +x ./build/cinnamon.sh
 chmod +x ./build/x86.sh
 chmod +x ./build/edge.sh
-chmod +x ./build/jetson.sh
